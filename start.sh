542698ee2b1ed66e170341f966ae3a46db603691
IHavenofriendss <96544729+IHavenofriendss@users.noreply.github.com>
2022-04-11 20:02:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/4kcinemas/My-Dream.git /My-Dream
+  git clone https://github.com/IHavenofriendss/Udayip22.git /Udayip22
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /My-Dream
+  git clone $UPSTREAM_REPO /Udayip22
 fi
 cd /My-Dream
 pip3 install -U -r requirements.txt
